aec3eebefdb77c183808ac7a2d1f6f60f3ba019f
Gam0re <146639059+Gam0re@users.noreply.github.com>
2024-04-11 21:02:36 +0300

    Update common.py

--- bot_template/handlers/common.py
+++ bot_template/handlers/common.py
@@ -2,11 +2,12 @@ from aiogram import types, Router
 
 from aiogram.filters import CommandStart
 
-import bot_template.keyboards.reply as kb
-
+import keyboards.reply as kb
+import database.requests as rq
 router = Router()
 
 #команда старт
 @router.message(CommandStart())
 async def start(message: types.Message):
+    await rq.set_user(message.from_user.id)
     await message.answer("Вас приветствует интернет магазин кондиционеров", reply_markup=kb.start)
