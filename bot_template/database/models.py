3e8a03fb49c3d83affea1f0730452380a5962f3b
Gam0re <146639059+Gam0re@users.noreply.github.com>
2024-04-11 21:02:05 +0300

    Add files via upload

---
+++ bot_template/database/models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import BigInteger, String, ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
+
+engine = create_async_engine(url='sqlite+aiosqlite:///db.sqlite3')
+
+async_session = async_sessionmaker(engine)
+
+class Base(AsyncAttrs, DeclarativeBase):
+    pass
+
+class User(Base):
+    __tablename__ = 'users'
+
+    __allow_unmapped__ = True
+    id: Mapped[int] = mapped_column(primary_key=True)
+    tg_id: Mapped[int] = mapped_column(BigInteger)
+    name: Mapped[str] = mapped_column(String(100), nullable=True)
+    phone: Mapped[int] = mapped_column(nullable=True)
+
+async def async_main():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
