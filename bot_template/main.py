31bf1f0e435dece1fc25d2ef68cef02886c0685b
Gam0re <146639059+Gam0re@users.noreply.github.com>
2024-04-11 21:03:37 +0300

    Update main.py

--- bot_template/main.py
+++ bot_template/main.py
@@ -2,9 +2,11 @@ import asyncio
 
 from aiogram import Bot, Dispatcher
 
-from bot_template.handlers.common import router
+from handlers.common import router
+from database.models import async_main
 
 async def main():
+    await async_main()
     bot = Bot(token="6626161631:AAH9Nro2O1J2SYb8mIUsX67WG2QviCDyAS4")
     dp = Dispatcher()
     dp.include_router(router)
