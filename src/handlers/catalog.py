f736b3419f46e9d6df3aaf20586d135d9bac0d56
Gam0re <olegivanov12398@gmail.com>
2024-04-16 22:38:06 +0300

    add levels

---
+++ src/handlers/catalog.py
@@ -0,0 +1,39 @@
+from aiogram import types, Router, F
+
+from aiogram.filters import Command
+
+import src.keyboards.inline.catalog as cl
+
+catalog_router = Router()
+
+
+# команда старт
+@catalog_router.message(Command('catalog'))
+@catalog_router.message(F.text == 'Каталог')
+async def catalog_lvl1(message: types.Message):
+    await message.answer('Выберете категорию товара', reply_markup=await cl.catalog_level_1())
+
+@catalog_router.callback_query(F.data == 'level_1')
+async def catalog_lvl2(callback: types.CallbackQuery):
+    await callback.answer('Вы выбрали категорию')
+    await callback.message.answer('Выберете категорию товара', reply_markup=await cl.catalog_level_2())
+
+@catalog_router.callback_query(F.data == 'level_2')
+async def catalog_lvl3(callback: types.CallbackQuery):
+    await callback.answer('Вы выбрали категорию')
+    await callback.message.answer('Выберете категорию товара', reply_markup=await cl.catalog_level_3())
+
+@catalog_router.callback_query(F.data == 'level_3')
+async def catalog_lvl4(callback: types.CallbackQuery):
+    await callback.answer('Вы выбрали категорию')
+    await callback.message.answer('Выберете категорию товара', reply_markup=await cl.catalog_level_4())
+
+@catalog_router.callback_query(F.data == 'level_4')
+async def catalog_lvl5(callback: types.CallbackQuery):
+    await callback.answer('Вы выбрали категорию')
+    await callback.message.answer('Выберете категорию товара', reply_markup=await cl.catalog_level_5())
+
+@catalog_router.callback_query(F.data == 'to_main')
+async def to_main(callback: types.CallbackQuery):
+    await callback.answer('На главную')
+    await callback.message.answer('Выберете категорию товара', reply_markup=await cl.catalog_level_5())
